3ee0195161c2ac1f5fd38ae74c28346d17275833
Emsun <emsunconcept@gmail.com>
2022-10-27 23:18:03 +0100

    created some components

---
+++ src/Component/LinkBtn.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const LinkBtn = () => {
+	return <div>LinkBtn</div>;
+};
---
+++ src/Component/ProfilePic.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ProfilePic = () => {
+	return <div>ProfilePic</div>;
+};
